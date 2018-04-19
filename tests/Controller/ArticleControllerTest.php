5197d7fc662356bfc07bda7fd41f12bda0bc3ef2
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-04-19 14:09:40 +0000

    Add phpunit

---
+++ tests/Controller/ArticleControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Controller\ArticleController;
+use PHPUnit\Framework\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+
+    public function testBla() {
+        $this->assertEquals("bla", "bla");
+    }
+}

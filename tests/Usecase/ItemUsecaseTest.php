5e493b09a44a264744f947e49ca3197f45c8f625
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-04-25 17:25:44 +0200

    Use a mock for the gateway

--- tests/Usecase/ItemUsecaseTest.php
+++ tests/Usecase/ItemUsecaseTest.php
@@ -4,17 +4,26 @@
 namespace App\Tests\Usecase;
 
 use App\CustomContext;
+use App\Gateway\BackendGateway;
 
-use App\Tests\Fakes\FakeGateway;
 use App\Usecase\ItemUsecase;
+use App\Model\Item;
+use App\Model\Reference;
 use PHPUnit\Framework\TestCase;
 
 class ItemUsecaseTest extends TestCase
 {
+    private $mockGateway;
 
-    public function testBla() {
+    function setUp() {
+        $this->mockGateway = $this->createMock(BackendGateway::class);
+        CustomContext::$backendGateway = $this->mockGateway;
+        
+    }
 
-        CustomContext::$backendGateway = new FakeGateway();
+    public function testFake() {
+
+        $this->configureMock();
 
         $itemUsecase = new ItemUsecase();
         $viewItem = $itemUsecase->constructItem("test");
@@ -22,5 +31,20 @@ class ItemUsecaseTest extends TestCase
         $this->assertEquals("fake_lemma", $viewItem->lemma);
         $this->assertEquals("next_fake_lemma", $viewItem->nextLemma);
         $this->assertEquals("previous_fake_lemma", $viewItem->previousLemma);
+    }
+
+    private function configureMock() {
+        $item = new Item();
+        $item->lemma = "fake_lemma";
+        $this->mockGateway->method('getItemById')->willReturn($item);
+
+        $nextRef = new Reference();
+        $nextRef->lemma = "next_fake_lemma";
+        $this->mockGateway->method('getNextReference')->willReturn($nextRef);
+        
+        $prevRef = new Reference();
+        $prevRef->lemma = "previous_fake_lemma";
+        $this->mockGateway->method('getPreviousReference')->willReturn($prevRef);
+        
     }
 }

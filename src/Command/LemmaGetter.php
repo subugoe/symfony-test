a8a879d65c1a27902b746a130a29bba0b9f6a76c
Dennis Neumann <neumann@sub.uni-goettingen.de>
2018-05-03 13:58:30 +0000

    Add commandline frontend

---
+++ src/Command/LemmaGetter.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Command;
+
+use App\CustomContext;
+use App\Gateway\SolrGateway;
+use App\Usecase\ItemUsecase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class LemmaGetter extends Command
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('getLemma')
+            ->setDescription('Gets an FWB article.')
+            ->addArgument("lemma-id");
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        CustomContext::$backendGateway = new SolrGateway();
+
+        $usecase = new ItemUsecase();
+        $lemmaId = $input->getArgument("lemma-id");
+        $result = $usecase->constructItem($lemmaId);
+
+        $output->writeln("Lemma: " . $result->lemma);
+    }
+}
